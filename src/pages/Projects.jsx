d5ce1ee493b8e62b3d26682cb27a7e1108b4c2a9
branhickman <branhickman@gmail.com>
2023-01-30 18:06:42 -0700

    configure ProjectList component to render within Projects

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,11 +1,16 @@
 import { projects } from "../data/projects";
+import ProjectList from "../components/ProjectList";
 
 const Projects = () => {
   return ( 
     <>
       <ul>
       {projects.map(project =>
-          <li key={project.title}>{project.title}</li>
+          <li key={project.title}>
+            <ProjectList 
+              project={project}
+            />
+          </li>
       )}
       </ul>
     </>
