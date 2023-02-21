2bf70c26a47b90001d75254d0d22a1314308b790
branhickman <branhickman@gmail.com>
2023-02-21 14:03:54 -0700

    update project images

--- src/data/projects.js
+++ src/data/projects.js
@@ -2,21 +2,21 @@ const projects = [
   {
     title: "Sharmagotchi",
     description: "Single page Javascript pet simulator based on the experiences of being a dog owner. Raise your pet by feeding, taking them out, and napping. Be careful not to let any of your meters get too full!",
-    image: "https://live.staticflickr.com/65535/52664472443_ae38c997d8_m.jpg",
+    image: "https://live.staticflickr.com/65535/52703569980_a6646c3e46_m.jpg",
     repositoryLink: "https://github.com/ClarkDoom/sharmagotchi",
     deploymentLink: "https://sharmagotchi.netlify.app/",
     technologyUsed: [ "javascript", "css", "html" ]
   },
     {title: "Syndicate",
     description: "Social media concept based around shared tv viewing interests. Track what you and your friends are watching, share reviews, and engage in interesting conversation",
-    image: "https://live.staticflickr.com/65535/52664472443_ae38c997d8_m.jpg",
+    image: "https://live.staticflickr.com/65535/52703637783_6d91c2ecfd_m.jpg",
     repositoryLink: "https://github.com/ClarkDoom/syndic8",
     deploymentLink: "https://syndicate.fly.dev/"
   },
   {
     title: "CAST",
     description: "This app was born out of a desire to optimize a very manual casting process. Instead of sifting through resumes one by one, Cast allows Talent to setup a profile and have Casting Directors reach out to them. It's more simple, streamined, and modern.",
-    image: "https://live.staticflickr.com/65535/52664472443_ae38c997d8_m.jpg",
+    image: "https://live.staticflickr.com/65535/52703570025_b93e400c64_m.jpg",
     repositoryLink: "https://github.com/hpweisberg/cast-front-end",
     deploymentLink: "https://castapp.netlify.app/",
     technologyUsed: [ "javascript", "css", "html" ]
@@ -24,7 +24,7 @@ const projects = [
   {
     title: "TEST",
     description: "This app was born out of a desire to optimize a very manual casting process. Instead of sifting through resumes one by one, Cast allows Talent to setup a profile and have Casting Directors reach out to them. It's more simple, streamined, and modern.",
-    image: "https://live.staticflickr.com/65535/52664472443_ae38c997d8_m.jpg",
+    image: "https://live.staticflickr.com/65535/52703409429_684b6e4d3c_m.jpg",
     repositoryLink: "https://github.com/hpweisberg/cast-front-end",
     deploymentLink: "https://castapp.netlify.app/",
     technologyUsed: [ "javascript", "css", "html" ]
