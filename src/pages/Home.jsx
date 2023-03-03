f7b6cb939f1cbdc8f854bdaf31a7d96869757805
branhickman <branhickman@gmail.com>
2023-03-02 17:49:28 -0700

    further changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,7 @@ import React from "react";
 import ReactGA from 'react-ga';
 const TRACKING_ID = "G-7MR4KGM2F9"; // YOUR_OWN_TRACKING_ID
 ReactGA.initialize(TRACKING_ID);
-
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 const Home = () => {
   return (
