d80bb3903acf32539c08177e2608bcea96765029
branhickman <branhickman@gmail.com>
2023-03-02 17:16:08 -0700

    add google analytics tracking via react-ga

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
 import { Routes, Route } from 'react-router';
+import ReactGA from 'react-ga';
+
 
 // components
 import Home from './pages/Home';
 import NavBar from './components/NavBar';
 
 function App() {
+
+  ReactGA.initialize('G-X8ERPGSYKZ')
+  ReactGA.pageview(window.location.pathname + window.location.search);
+
   return (
     <div className="App">
       <NavBar />
