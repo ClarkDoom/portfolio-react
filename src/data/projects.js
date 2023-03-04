3d9b2f06186b161255a32c2a829660c378e85bb0
branhickman <branhickman@gmail.com>
2023-03-03 19:42:38 -0700

    additional project card css changes

--- src/data/projects.js
+++ src/data/projects.js
@@ -2,14 +2,14 @@ const projects = [
   {
     title: "Sharmagotchi",
     description: "Single page Javascript pet simulator based on the experiences of being a dog owner. Raise your pet by feeding, taking them out, and napping. Be careful not to let any of your meters get too full!",
-    image: "https://live.staticflickr.com/65535/52703569980_a6646c3e46_m.jpg",
+    image: "https://live.staticflickr.com/65535/52724890273_b593d97391_o.png",
     repositoryLink: "https://github.com/ClarkDoom/sharmagotchi",
     deploymentLink: "https://sharmagotchi.netlify.app/",
     technologyUsed: [ {name: "/icons/javascript.png"}, {name: "/icons/css.png"}, {name: "/icons/html.png"} ]
   },
     {title: "Syndicate",
     description: "Social media concept based around shared tv viewing interests. Track what you and your friends are watching, share reviews, and engage in interesting conversation",
-    image: "https://live.staticflickr.com/65535/52703637783_6d91c2ecfd_m.jpg",
+    image: "https://live.staticflickr.com/65535/52724668299_3d43f28896_o.png",
     repositoryLink: "https://github.com/ClarkDoom/syndic8",
     deploymentLink: "https://syndicate.fly.dev/",
     technologyUsed: [ {name: "/icons/javascript.png"}, {name: "/icons/css.png"}, {name: "/icons/html.png"}, {name: '/icons/ejs.png'}, {name: '/icons/bootstrap.png'}, {name: '/icons/node.png'}, {name: '/icons/express.png'}, {name: '/icons/flyio.png'}, {name: '/icons/googleauth.png'} ]
@@ -17,10 +17,19 @@ const projects = [
   {
     title: "CAST",
     description: "This app was born out of a desire to optimize a very manual casting process. Instead of sifting through resumes one by one, Cast allows Talent to setup a profile and have Casting Directors reach out to them. It's more simple, streamined, and modern.",
-    image: "https://live.staticflickr.com/65535/52703570025_b93e400c64_m.jpg",
+    image: "https://live.staticflickr.com/65535/52724413636_47e7d003d4_o.png",
     repositoryLink: "https://github.com/hpweisberg/cast-front-end",
     deploymentLink: "https://castapp.netlify.app/",
     technologyUsed: [ {name: "/icons/javascript.png"}, {name: "/icons/css.png"}, {name: "/icons/html.png"}, {name: "/icons/react.png"}, {name: "/icons/mongoose.png"}, {name: "/icons/node.png"}, {name: "/icons/express.png"}, {name: "/icons/netlify.png"}, {name: "/icons/flyio.png"} ]
+  },
+  {
+    title: "SELL",
+    description: "Used media marketplace that differentiates from other ecommerce apps by putting a focus on trading as well as selling/buying.",
+    image: "https://live.staticflickr.com/65535/52724808600_ef9c59fca5_o.png",
+    repositoryLink: "https://github.com/ClarkDoom/sell-front-end",
+    deploymentLink: "https://sell-react-ts.netlify.app/",
+    technologyUsed: [ {name: "/icons/javascript.png"}, {name: "/icons/css.png"}, {name: "/icons/html.png"}, {name: "/icons/react.png"}, {name: "/icons/node.png"}, {name: "/icons/express.png"}, {name: "/icons/netlify.png"}, {name: "/icons/flyio.png"},{name: "/icons/postgres.png"},{name: "/icons/sequelize.png"}
+  ]
   },
 ]
 
